f3b81f13795b532dd2e96850c5145f478650ed94
Sayado01 <y.handane@gmail.com>
2026-04-14 23:33:02 +0200

    update DataControllers for uploading file: managing file name

--- controllers/DataController.py
+++ controllers/DataController.py
@@ -1,7 +1,13 @@
 
+from .ProjectController import ProjectController
 from .BaseController import BaseController
 from fastapi import UploadFile
 from models import ResponseSignal
+import re
+import os
+
+
+
 class DataController(BaseController):
     
     def __init__(self):
@@ -19,4 +25,30 @@ class DataController(BaseController):
     
     
     def generate_unique_filename(self , orig_file_name : str , project_id : str):
+        random_key = self.generate_random_string()
+        project_path = ProjectController().get_project_path(
+            project_id=project_id
+        )
         
+        cleaned_file_name = self.get_clean_file_name(orig_file_name=orig_file_name)
+        
+        new_file_path = os.path.join(
+            project_path,
+            random_key + "_" +cleaned_file_name
+        )
+        
+        while os.path.exists(new_file_path):
+            random_key = self.generate_random_string()
+            new_file_path = os.path.join(
+            project_path,
+            random_key + "_" +cleaned_file_name
+        )
+            
+        return new_file_path
+    
+    
+    def get_clean_file_name(self , orig_file_name :str):
+        
+        ## remove any special characters , except underscores and .
+        cleaned_file_name = re.sub(r'[^\w.]', ''  , orig_file_name.strip())
+        return cleaned_file_name
