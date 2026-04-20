ff382a358dfeb01b106a74f8c881b98b59405c7a
Sayado01 <y.handane@gmail.com>
2026-04-20 18:37:56 +0200

    add mongo connexion

--- src/main.py
+++ src/main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
 from routes import base , data
-
-
+from motor.motor_asyncio import AsyncIOMotorClient
+from helpers.config import get_settings
+from contextlib import asynccontextmanager
 app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # --- Startup ---
+    settings = get_settings()
+    
+    # On attache directement à app
+    app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
+    app.mongo_db = app.mongo_conn[settings.MONGODB_DATABASE]
+    
+    yield
+    
+    # --- Shutdown ---
+    app.mongo_conn.close()
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
