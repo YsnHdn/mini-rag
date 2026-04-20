8a2e82af375874ea71ab8ece079df3fbc8b910a1
Sayado01 <y.handane@gmail.com>
2026-04-20 17:04:24 +0200

    add a new branch for file processing following a defined schema

--- routes/data.py
+++ routes/data.py
@@ -3,9 +3,10 @@ from fastapi import FastAPI , APIRouter , Depends , UploadFile , status
 from fastapi.responses import JSONResponse
 import os
 from helpers.config import get_settings , Settings
-from controllers import DataController , ProjectController
+from controllers import DataController , ProjectController , ProcessController
 from models import ResponseSignal
 import logging
+from .schemes.data import ProcessRequest
 
 logger = logging.getLogger('uvicorn.error')
 
@@ -34,7 +35,7 @@ async def upload_data(project_id: str , file : UploadFile,app_settings : Setting
         )
     
     #project_dir_path = ProjectController().get_project_path(project_id=project_id)
-    file_path = data_controller.generate_unique_filename(
+    file_path , file_id = data_controller.generate_unique_filepath(
         orig_file_name=file.filename,
         project_id=project_id
     )
@@ -54,6 +55,33 @@ async def upload_data(project_id: str , file : UploadFile,app_settings : Setting
         )
     return JSONResponse(
             content={
-                "signal" : ResponseSignal.FILE_UPLOAD_SUCCESS.value
+                "signal" : ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+                "file_id" : file_id
             }
         )
+    
+    
+@data_router.post("/process/{project_id}")
+async def process_endpoint(project_id:str, process_request  : ProcessRequest):
+    
+    file_id = process_request.file_id
+    chunk_size = process_request.chunk_size
+    overlap_size = process_request.overlap_size
+    
+    print(f"chunk_size : {chunk_size} ; overlap_size : {overlap_size}")
+    process_controller = ProcessController(project_id=project_id)
+    file_content = process_controller.get_file_content(file_id=file_id)
+    file_chunks = process_controller.process_file_content(
+        file_content=file_content,
+        chunk_size=chunk_size ,
+        overlap_size=overlap_size)
+    
+    if file_chunks is None or len(file_chunks)==0:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={
+                'signal' : ResponseSignal.PROCESSING_FAILED.value
+            }
+        )
+    
+    return file_chunks
