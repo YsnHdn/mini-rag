319dc9011af116d975384b178e25cd5236d9d92c
Sayado01 <y.handane@gmail.com>
2026-04-20 18:36:50 +0200

    add Mongo URL & DATABASE

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -11,6 +11,9 @@ class Settings(BaseSettings):
     FILE_MAX_SIZE : int
     FILE_DEFAULT_CHUNK_SIZE : int
     
+    MONGODB_URL : str
+    MONGODB_DATABASE:str
+    
     class Config: ## Config is mandatory , only Pydantic understands it 
         env_file = ".env" ## same here
         
